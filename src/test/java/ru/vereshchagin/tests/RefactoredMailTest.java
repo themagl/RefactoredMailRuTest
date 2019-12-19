2e84d4f310cfb7f8e68d6b4689a756eb7483b6e7
Artem_Vereshchagin <Your_Email>
2019-12-19 12:54:44 +0300

    Добавлен pom проекта

--- src/test/java/ru/vereshchagin/tests/RefactoredMailTest.java
+++ src/test/java/ru/vereshchagin/tests/RefactoredMailTest.java
@@ -1,4 +1,23 @@
 package ru.vereshchagin.tests;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.timeout;
+import static com.codeborne.selenide.Configuration.startMaximized;
+
 public class RefactoredMailTest {
+
+    @BeforeEach
+    public void openBrowser(){
+        timeout = 10000;
+        startMaximized = true;
+        browser = "chrome";
+    }
+
+    @AfterEach
+    public void closeBrowser(){
+        closeBrowser();
+    }
 }
